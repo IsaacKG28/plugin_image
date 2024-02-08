d218e0ca099e72987a65debfde25975d2c03082b
IsaacKG28 <158130267+IsaacKG28@users.noreply.github.com>
2024-02-08 17:48:54 -0600

    Update image-checker.php
    Ya he agregado la opción de habilitar el botón nuevamente. Pero no regresa a deshabilitarlo

--- image-checker.php
+++ image-checker.php
@@ -1,31 +1,35 @@
 <?php
 /*
-Plugin Name: Disable Publish Button
-Description: Deshabilita el botón de publicar producto en WooCommerce si no hay imágenes en la galería.
+Plugin Name: Deshabilitar Publicar Sin Imágenes
+Description: Deshabilita el botón de publicar en productos de WooCommerce si no hay imágenes en la galería.
 Version: 1.0
 Author: Fernando Isaac Gonzalez Medina
 */
 
 add_action('admin_footer', 'disable_publish_button');
+
 function disable_publish_button() {
     global $post;
-    if($post->post_type == 'product') { // verifica si el tipo de post es 'product'
-        $gallery_images = get_post_meta($post->ID, '_product_image_gallery', true);
-        if(empty($gallery_images)) { // verifica si la galería de imágenes está vacía
-            ?>
-            <script type="text/javascript">
-                jQuery(document).ready(function($) {
-                    $('#publish').prop('disabled', true); // deshabilita el botón de publicar
-                    $('body').on('change', '.wp-picker-input-wrap input', function() { // escucha los cambios en la galería de imágenes
-                        var gallery_images = $('#_product_image_gallery').val();
-                        if(gallery_images != '') {
-                            $('#publish').prop('disabled', false); // habilita el botón de publicar si la galería de imágenes no está vacía
-                        }
-                    });
+    if($post->post_type == 'product') { // Verifica si el tipo de post es 'product'
+        ?>
+        <script type="text/javascript">
+            jQuery(document).ready(function($) {
+                var gallery_images = $('.product_images.ui-sortable li').length;
+                if(gallery_images == 0) { // Verifica si la galería de imágenes está vacía
+                    $('#publish').prop('disabled', true); // Deshabilita el botón de publicar
+                }
+                $('body').on('DOMNodeInserted', '.product_images.ui-sortable li', function () {
+                    $('#publish').prop('disabled', false); // Habilita el botón de publicar si se agrega una imagen a la galería
                 });
-            </script>
-            <?php
-        }
+                $('body').on('DOMNodeRemoved', '.product_images.ui-sortable li', function () {
+                    if($('.product_images.ui-sortable li').length == 0) {
+                        $('#publish').prop('disabled', true); // Deshabilita el botón de publicar si se eliminan todas las imágenes de la galería
+                    }
+                });
+            });
+        </script>
+        <?php
     }
 }
+
 ?>
