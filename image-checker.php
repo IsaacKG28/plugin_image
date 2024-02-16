50c3251d744a849f8d1293a9dfafed422d28d61d
IsaacKG28 <158130267+IsaacKG28@users.noreply.github.com>
2024-02-16 12:08:39 -0600

    Update image-checker.php
    He agregago hooks para cambiar el estado en tiempo real, aunque aun no tiene más condicionales y comprobaciones, ya funciona

--- image-checker.php
+++ image-checker.php
@@ -5,15 +5,27 @@ Description: Deshabilita el botón de publicar en productos de WooCommerce si no
 Version: 1.0
 Author: Fernando Isaac Gonzalez Medina
 */
+//Acción que cambia un producto publicado a borrador si no tiene imágenes
+add_action('save_post', 'check_product_images', 10, 3);
+function check_product_images($post_id, $post, $update) {
+    if ($post->post_type == 'product' && $post->post_status == 'publish') {
+        $product = wc_get_product($post_id);
+        $attachment_ids = $product->get_gallery_image_ids();
+        if (empty($attachment_ids)) {
+            // Cambiamos el estado a 'Borrador'
+            $post->post_status = 'draft';
+            wp_update_post($post);
+        }
+    }
+}
+//acción para productos en woocommerce
 add_action('admin_footer', 'disable_publish_button');
 function disable_publish_button() {
     global $post;
-
     // Verifica si $post es una instancia válida de WP_Post
     if (!is_a($post, 'WP_Post')) {
         return; // Salir de la función si $post no es válido
     }
-
     // Ahora puedes acceder a las propiedades de $post de manera segura
     if ($post->post_type == 'product') { // Verifica si el tipo de post es 'product'
         ?>
@@ -39,20 +51,18 @@ function disable_publish_button() {
         </script>
         <?php
     }
-}
-//Accion para BEAR BULK EDITOR
-add_action('save_post', 'check_product_images', 10, 3);
-function check_product_images($post_id, $post, $update) {
-    if ($post->post_type == 'product' && $post->post_status == 'publish') {
-        $product = wc_get_product($post_id);
-        $attachment_ids = $product->get_gallery_image_ids();
-        if (empty($attachment_ids)) {
-            // Cambiamos el estado a 'Borrador'
-            $post->post_status = 'draft';
-            wp_update_post($post);
-        }
-    }
 } 
+//Parte que hookea correctamente BEAR para cambiar el status en tiempo real
+add_filter('woobe_new_product_status', function ($status) {
+    // Verificar si la galería está vacía
+    if (empty(get_post_gallery(get_the_ID(), false))) {
+        // La galería está vacía, establecer el estado como 'draft'
+        return 'draft';
+    } else {
+        // La galería no está vacía, mantener el estado actual
+        return $status;
+    }
+});
 add_action('admin_enqueue_scripts', 'enqueue_my_custom_popup_script');
 function enqueue_my_custom_popup_script() {
     $screen = get_current_screen();
@@ -60,15 +70,13 @@ function enqueue_my_custom_popup_script() {
         wp_enqueue_script('my_custom_popup_script', plugins_url('/my_custom_popup.js', __FILE__), array('jquery', 'thickbox'), false, true);
     }
 }
-
 add_action('admin_footer', 'my_custom_popup');
 function my_custom_popup() {
     $screen = get_current_screen();
-    if ( $screen->id == "product_page_woobe" ) {
-        echo '<div id="my_custom_popup" style="display: none;">
+    if ( $screen->id == "product_page_woobe") {
+        echo '<div id="my_custom_popup">
                 <p>Recuerda que si pones como "Publicado" un artículo sin imágenes en la galería, éste se cambiará automáticamente a "Borrador".</p>
               </div>';
     }
 }
-
 ?>
