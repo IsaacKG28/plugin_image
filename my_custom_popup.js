48494eb8a446dd923078f3649511144fadf928d4
IsaacKG28 <158130267+IsaacKG28@users.noreply.github.com>
2024-02-21 19:49:44 -0600

    Update my_custom_popup.js

--- my_custom_popup.js
+++ my_custom_popup.js
@@ -1,8 +1,3 @@
 jQuery(document).ready(function($) {
-    // Asegúrate de que Thickbox esté cargado antes de ejecutar tb_show
-    if (typeof tb_show === 'function') {
-        tb_show("Recordatorio", "#TB_inline?width=600&height=550&inlineId=my_custom_popup");
-    } else {
-        console.error('Thickbox no está cargado correctamente.');
-    }
+    tb_show("Recordatorio", "#TB_inline?width=600&height=550&inlineId=my_custom_popup");
 });
